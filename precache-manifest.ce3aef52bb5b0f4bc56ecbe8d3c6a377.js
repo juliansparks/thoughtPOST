c6e5104cbd7b557cf22438a2a579a9d81b164521
Julian Uquillas <juqui001@fiu.edu>
2019-02-08 18:26:11 -0500

    Updates

---
+++ precache-manifest.ce3aef52bb5b0f4bc56ecbe8d3c6a377.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "45598562077bdc84281b5330ff4d283e",
+    "url": "/thoughtPOST/static/media/heart.45598562.png"
+  },
+  {
+    "revision": "5182da425f811908bed9f5b8c72fa44f",
+    "url": "/thoughtPOST/static/media/SourceSansPro-Regular.5182da42.ttf"
+  },
+  {
+    "revision": "01782a7fff27bc527f8af8095aac1098",
+    "url": "/thoughtPOST/static/media/ShadowsIntoLight.01782a7f.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/thoughtPOST/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "690d1dc1dc2bf16907b4",
+    "url": "/thoughtPOST/static/js/runtime~main.690d1dc1.js"
+  },
+  {
+    "revision": "9f17271767aa402e570a",
+    "url": "/thoughtPOST/static/js/main.9f172717.chunk.js"
+  },
+  {
+    "revision": "a972d6d0cd85d26c49a9",
+    "url": "/thoughtPOST/static/js/1.a972d6d0.chunk.js"
+  },
+  {
+    "revision": "9f17271767aa402e570a",
+    "url": "/thoughtPOST/static/css/main.41ed5452.chunk.css"
+  },
+  {
+    "revision": "9d703af4bd1d0a81f12e6d8782e59761",
+    "url": "/thoughtPOST/index.html"
+  }
+];
